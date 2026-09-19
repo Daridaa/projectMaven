4f4f1a75466738e24a4ad89a8b040ad6e6d53585
Dari <sirapovadari@gmail.com>
2026-09-19 15:05:00 +0800

    Ваше сообщение о коммите

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Mylib.factorial(5));
+        System.out.println(Mylib.prime(17));
+        System.out.println(Mylib.gcd(18,48));
+        System.out.println(Mylib.lcm(18,48));
+    }
+}
